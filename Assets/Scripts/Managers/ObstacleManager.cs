31b81ae7fbaf1075a4b0cc79e06fe3ebc55c61c3
Muhammet Yasin YILDIZ <mysnyldz@gmail.com>
2022-07-22 20:16:29 +0300

    Added Obstacle Manager and Obstacle

---
+++ Assets/Scripts/Managers/ObstacleManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using Signals;
+using DG.Tweening;
+
+namespace Managers
+{
+    public class ObstacleManager : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Private Variables
+
+        private DOTweenAnimation _obAnim;
+
+        #endregion
+
+        
+        #endregion
+        
+        #region Event Subscriptions
+
+        private void OnEnable()
+                {
+                    SubscribeEvents();
+                }
+                private void SubscribeEvents()
+                {
+                    CoreGameSignals.Instance.onPlay += OnObstacleAnimationStart;
+                }
+        
+                private void UnsubscribeEvents()
+                {
+                    CoreGameSignals.Instance.onPlay -= OnObstacleAnimationStart;
+                }
+        
+                private void OnDisable()
+                {
+                    UnsubscribeEvents();
+                }
+        
+
+        #endregion
+        
+        
+        
+        private void Awake()
+        {
+            _obAnim = this.GetComponent<DOTweenAnimation>();
+        }
+
+        private void OnObstacleAnimationStart()
+        {
+            Debug.Log("Running");
+            _obAnim.DOPlay();
+        }  
+    }
+}
